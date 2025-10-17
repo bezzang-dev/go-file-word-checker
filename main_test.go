109b638f5cb8bd7e60eb1323eca71d5f3c28d840
bezzang-dev <kjmdkdlel@gmail.com>
2025-10-18 01:54:19 +0900

    use stretchr/testify

--- main_test.go
+++ main_test.go
@@ -4,22 +4,21 @@ import (
 	"os"
 	"sync"
 	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFindWordInFile(t *testing.T) {
 	content := []byte("hello world\nthis is a test\nfind the word hello here\nend of file")
 	tmpfile, err := os.CreateTemp("", "testfile_*.txt")
-	if err != nil {
-		t.Fatalf("Failed to create temp file: %v", err)
-	}
+	require.NoError(t, err, "Failed to create temp file")
 	defer os.Remove(tmpfile.Name())
 
-	if _, err := tmpfile.Write(content); err != nil {
-		t.Fatalf("Failed to write to temp file: %v", err)
-	}
-	if err := tmpfile.Close(); err != nil {
-		t.Fatalf("Failed to close temp file: %v", err)
-	}
+	_, err = tmpfile.Write(content)
+	require.NoError(t, err, "Failed to create temp file")
+	
+	err = tmpfile.Close()
+	require.NoError(t, err, "Failed to close temp file")
 
 	var wg sync.WaitGroup
 	ch := make(chan FindInfo, 1)
@@ -36,16 +35,10 @@ func TestFindWordInFile(t *testing.T) {
 
 	result := <-ch
 
-	if len(result.lines) != expectedLine {
-		t.Errorf("Expected to find %d lines, but got %d", expectedLine, len(result.lines))
-	}
-	if result.lines[0].lineNo != 1 {
-		t.Errorf("Expected line number to be 1, but got %d", result.lines[0].lineNo)
-	}
-	if result.lines[0].line != "hello world" {
-		t.Errorf("Expected line content to be 'hello wrold', but got '%s'", result.lines[0].line)
-	}
-	if result.lines[1].lineNo != 3 {
-		t.Errorf("Expected line content to be 3, but got '%d'", result.lines[1].lineNo)
-	}
+	assert.Equal(t, expectedLine, len(result.lines), "Expected to find %d lines, but got %d", expectedLine, len(result.lines))
+
+	assert.Equal(t, 1, result.lines[0].lineNo, "Expected line number to be 1, but got %d", result.lines[0].lineNo)
+	assert.Equal(t, "hello world", result.lines[0].line, "Expected line content to be 'hello wrold', but got '%s'", result.lines[0].line)
+	
+	assert.Equal(t, 3, result.lines[1].lineNo, "Second match line number should be 3")
 }
